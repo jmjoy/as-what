01753c3cb6cf520dbccca2333fa7674c9d16d57d
jmjoy <jmjoy@apache.org>
2025-05-03 19:28:31 +0800

    Add release workflow and update trait implementations for char type

--- src/lib.rs
+++ src/lib.rs
@@ -24,73 +24,73 @@ as_trait!(
     i8,
     AsI8,
     as_i8,
-    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64]
+    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, char]
 );
 as_trait!(
     i16,
     AsI16,
     as_i16,
-    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64]
+    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, char]
 );
 as_trait!(
     i32,
     AsI32,
     as_i32,
-    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64]
+    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, char]
 );
 as_trait!(
     i64,
     AsI64,
     as_i64,
-    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64]
+    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, char]
 );
 as_trait!(
     i128,
     AsI128,
     as_i128,
-    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64]
+    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, char]
 );
 as_trait!(
     isize,
     AsIsize,
     as_isize,
-    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64]
+    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, char]
 );
 as_trait!(
     u8,
     AsU8,
     as_u8,
-    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64]
+    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, char]
 );
 as_trait!(
     u16,
     AsU16,
     as_u16,
-    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64]
+    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, char]
 );
 as_trait!(
     u32,
     AsU32,
     as_u32,
-    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64]
+    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, char]
 );
 as_trait!(
     u64,
     AsU64,
     as_u64,
-    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64]
+    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, char]
 );
 as_trait!(
     u128,
     AsU128,
     as_u128,
-    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64]
+    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, char]
 );
 as_trait!(
     usize,
     AsUsize,
     as_usize,
-    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64]
+    [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, char]
 );
 as_trait!(
     f32,
@@ -103,4 +103,10 @@ as_trait!(
     AsF64,
     as_f64,
     [i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64]
+);
+as_trait!(
+    char,
+    AsChar,
+    as_char,
+    [u8, char]
 );
